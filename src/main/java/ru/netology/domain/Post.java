3d1e519d921b69086f1c4d82ea221a9ceebfdf9e
Oleg Georgiyevski <oleqg@yahoo.com>
2020-07-04 18:26:25 +0300

    Classes added, and modified

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,13 +1,30 @@
 package ru.netology.domain;
 
 public class Post {
-    public String ownerTitleUrl;
-    private String imageUrl;
-    private int view;
-    private UserAction useraction;
-    private Comments comments;
     private int id;
-    private String description;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private boolean friendsOnly;
+    private String imageUrl;
+    private String copyright;
+    private Views views;
+    private String postType;
+    private int signerId;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
+    private boolean postponedId;
+    private CommentsInfo commentsInfo;
+    private Likes likes;
+    private Geo geo;
+    private Reposts reposts;
 
     public int getId() {
         return id;
@@ -17,22 +34,60 @@ public class Post {
         this.id = id;
     }
 
-
-
-    public String getOwnerTitleUrl() {
-        return ownerTitleUrl;
+    public int getOwnerId() {
+        return ownerId;
     }
 
-    public void setOwnerTitleUrl(String ownerTitleUrl) {
-        this.ownerTitleUrl = ownerTitleUrl;
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
     }
 
-    public String getDescription() {
-        return description;
+    public int getFromId() {
+        return fromId;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public boolean isFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(boolean friendsOnly) {
+        this.friendsOnly = friendsOnly;
     }
 
     public String getImageUrl() {
@@ -43,30 +98,123 @@ public class Post {
         this.imageUrl = imageUrl;
     }
 
-    public int getView() {
-        return view;
+    public Views getViews() {
+        return views;
     }
 
-    public void setView(int view) {
-        this.view = view;
+    public void setViews(Views views) {
+        this.views = views;
     }
 
-
-    public UserAction getUseraction() {
-        return useraction;
+    public String getPostType() {
+        return postType;
     }
 
-    public void setUseraction(UserAction useraction) {
-        this.useraction = useraction;
+    public void setPostType(String postType) {
+        this.postType = postType;
     }
 
-    public Comments getComments() {
-        return comments;
+    public int getSignerId() {
+        return signerId;
     }
 
-    public void setComments(Comments comments) {
-        this.comments = comments;
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
     }
 
+    public boolean isCanPin() {
+        return canPin;
+    }
 
+    public void setCanPin(boolean canPin) {
+        this.canPin = canPin;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public boolean isMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(boolean markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public boolean isPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(boolean postponedId) {
+        this.postponedId = postponedId;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public Likes getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Likes likes) {
+        this.likes = likes;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public Reposts getReposts() {
+        return reposts;
+    }
+
+    public void setReposts(Reposts reposts) {
+        this.reposts = reposts;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
 }
---
+++ src/main/java/ru/netology/domain/Reposts.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+public class Reposts {
+
+    private int count;
+    private boolean userReposted;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isUserReposted() {
+        return userReposted;
+    }
+
+    public void setUserReposted(boolean userReposted) {
+        this.userReposted = userReposted;
+    }
+}
